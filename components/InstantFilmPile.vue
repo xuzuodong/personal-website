4dcfdfec350ab47923c760a91a0416bf1069dfa5
许作栋 <hi@xzd.me>
2023-12-16 22:56:51 +0800

    chore: Add InstantFilmFrame component and update styles

--- components/InstantFilmPile.vue
+++ components/InstantFilmPile.vue
@@ -25,18 +25,14 @@ function moveCardOutOfScreen(i: number, movementX: number, vx: number) {
     motionInstance.apply({ x, rotate, transition: { stiffness: 120, damping: 20 } })
 }
 
-function moveCardBack(i: number) {
-    const motionInstance = motions[`motion-${i}`]
-    motionInstance.apply({ x: 0, y: 0, scale: 1, transition: { stiffness: 120, damping: 15 } })
-}
-
-function moveCardWithCursor(i: number, movementX: number) {
-    const motionInstance = motions[`motion-${i}`]
-    motionInstance.apply({ x: movementX, scale: 1.1, rotate: 0, transition: { stiffness: 400, damping: 20 } })
-}
-
 let draggingInfo: null | { i: number, movementX: number, vx: number, cancel: () => void } = null
 
+/**
+ The `@vueuse/gesture` package has an issue where the drag interaction remains unfinished
+ if the user drags too quickly, causing the card to lag behind the pointer's movement,
+ and releases the mouse before the interaction can catch up.
+ To address this, we listen to the document's `pointerup` event to ensure the interaction is properly completed.
+ */
 useEventListener(document, 'pointerup', () => {
     if (draggingInfo) {
         const { i, movementX, vx, cancel } = draggingInfo
@@ -48,8 +44,9 @@ useEventListener(document, 'pointerup', () => {
 })
 
 function handleDrag(i: number) {
-    return function (state: Parameters<Handler<'drag'>>[0]) {
-        const { movement, dragging, velocities: [vx], cancel } = state
+    return function (dragState: Parameters<Handler<'drag'>>[0]) {
+        const { movement, dragging, velocities: [vx], cancel } = dragState
+
         const motionInstance = motions[`motion-${i}`]
         if (!motionInstance) return
 
@@ -61,14 +58,16 @@ function handleDrag(i: number) {
             moveCardOutOfScreen(i, movement[0], vx)
         }
 
+        // move the card back to its original position
         else if (!dragging) {
             draggingInfo = null
-            moveCardBack(i)
+            motionInstance.apply({ x: 0, y: 0, scale: 1, rotate: 0, transition: { stiffness: 120, damping: 15 } })
         }
 
+        // keep the card moving with the pointer
         else {
             draggingInfo = { i, movementX: movement[0], vx, cancel }
-            moveCardWithCursor(i, movement[0])
+            motionInstance.apply({ x: movement[0], scale: 1.1, rotate: 0, transition: { stiffness: 400, damping: 20 } })
         }
     }
 }
@@ -85,14 +84,13 @@ whenever(() => goneCards.size === props.instantFilms.length, async () => {
     <div class="relative h-[500px]">
         <div
             v-for="(card, i) in instantFilms" :key="card.image.asset._id"
-            class="instant-film-container"
+            class="film-container"
         >
-            <div
+            <instant-film-frame
                 v-motion="`motion-${i}`"
                 v-drag="handleDrag(i)"
                 :initial="{ scale: 1.5, y: -1000 }"
                 :enter="enterVariant(i)"
-                class="instant-film"
                 :class="goneCards.size === instantFilms.length - i - 1 ? 'pointer-events-auto' : 'pointer-events-none'"
             >
                 <sanity-image
@@ -102,30 +100,18 @@ whenever(() => goneCards.size === props.instantFilms.length, async () => {
                             ? 'w-[256px] h-[192px] lg:w-[320px] lg:h-[240px]'
                             : 'w-[192px] h-[256px] lg:w-[240px] lg:h-[320px]',
                     ]"
-                    class="image"
+                    class="object-cover pointer-events-none touch-none select-none"
                 />
-            </div>
+            </instant-film-frame>
         </div>
     </div>
 </template>
 
-<style scoped>
-.instant-film-container {
+<style lang="scss" scoped>
+.film-container {
     @apply absolute w-full h-full
         flex justify-center items-center
         pointer-events-none select-none touch-none
         will-change-transform;
-}
-
-.instant-film {
-    @apply bg-white border-white
-        border-[0.8em] border-b-[3.2em] lg:border-[1em] lg:border-b-[4em] rounded-[3px]
-        will-change-transform;
-    box-shadow: 0 12.5px 100px -10px rgba(50, 50, 73, 0.4), 0 10px 10px -10px rgba(50, 50, 73, 0.3);
-    filter: sepia(0.05) brightness(0.9);
-}
-
-.image {
-    @apply object-cover pointer-events-none touch-none select-none;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -22,33 +22,37 @@ const instantFilms = computed(() => data.value?.[0].images.map(image => ({
 </script>
 
 <template>
-    <div
-        class="
-          min-h-[540px] container mx-auto
-          flex flex-col-reverse lg:flex-row
-          [&>*]:w-full lg:[&>*]:basis-1/2 h-3/5 items-center
-        "
-    >
-        <div class="h-full px-4 flex flex-col justify-around">
-            <div class="flex items-center space-x-1">
-                <icon name="tdesign:location" />
-                <p>Hangzhou, China</p>
+    <div class="overflow-x-hidden">
+        <div
+            class="
+                min-h-[calc(max(540px,60%))] container mx-auto
+                flex flex-col-reverse lg:flex-row
+                [&>*]:w-full
+            "
+        >
+            <div class="px-4 lg:px-6 xl:px-8 flex flex-col justify-center lg:basis-1/2 xl:basis-3/5">
+                <div class="hidden lg:flex items-center space-x-1">
+                    <icon name="tdesign:location" />
+                    <p>Hangzhou, China</p>
+                </div>
+
+                <div class="h-3/5 flex flex-col justify-center space-y-6">
+                    <h1 class="text-center lg:text-left text-2xl lg:text-4xl xl:text-5xl lg:leading-tight xl:leading-[1.3] font-semibold">
+                        Scripting realities in bytes, <br />
+                        Painting tales with light.
+                    </h1>
+
+                    <p class="text-sm text-center lg:text-left lg:text-base lg:leading-snug">
+                        I'm a front-end engineer and photographer who loves creating captivating websites and capturing beautiful moments. Explore my creations on this website and enjoy!
+                    </p>
+                </div>
             </div>
 
-            <h1 class="text-center lg:text-justify text-lg lg:text-3xl font-semibold">
-                Scripting realities in bytes, <br />
-                Painting tales with light.
-            </h1>
-
-            <p class="text-sm lg:text-base leading-snug">
-                I'm a front-end engineer and photographer who loves creating captivating websites and capturing beautiful moments. Explore my creations on this website and enjoy!
-            </p>
-        </div>
-
-        <div>
-            <client-only v-if="instantFilms">
-                <instant-film-pile :instant-films="instantFilms" />
-            </client-only>
+            <div class="self-center lg:basis-1/2 xl:basis-2/5">
+                <client-only v-if="instantFilms">
+                    <instant-film-pile :instant-films="instantFilms" />
+                </client-only>
+            </div>
         </div>
     </div>
 </template>
