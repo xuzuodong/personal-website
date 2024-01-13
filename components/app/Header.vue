9ba799b9f9e4b95f13118151cdd3525518bdb9f5
xuzuodong <hi@xzd.me>
2024-01-13 16:57:56 +0800

    chore(wip): impl burger

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-
+const navItems = [
+    { name: 'Photography', path: '/photography' },
+    { name: 'Projects', path: '/projects' },
+    { name: 'About', path: '/about' },
+]
 </script>
 
 <template>
     <div class="dark:border-b dark:border-gray-300 dark:border-opacity-30 h-[60px] shadow-lg dark:shadow-none">
-        <div class="h-full container flex justify-between items-center">
+        <div class="hidden lg:flex h-full container justify-between items-center">
             <nuxt-link to="/">
                 <nuxt-img src="/avatar.jpg" width="40" height="40" class="rounded-full" />
             </nuxt-link>
 
+            <div class="flex items-center space-x-4">
+                <nuxt-link
+                    v-for="item in navItems"
+                    :key="item.path"
+                    :to="item.path"
+                    class="text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+                    active-class="text-gray-900 dark:text-gray-100"
+                    exact
+                >
+                    {{ item.name }}
+                </nuxt-link>
+            </div>
+
             <client-only>
                 <base-switch-theme />
             </client-only>
+        </div>
+
+        <div class="px-4 h-full flex lg:hidden justify-between items-center">
+            <nuxt-link to="/">
+                <nuxt-img src="/avatar.jpg" width="32" height="32" class="rounded-full" />
+            </nuxt-link>
+
+            <app-header-burger />
         </div>
     </div>
 </template>
---
+++ components/app/HeaderBurger.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+const modelValue = ref(false)
+</script>
+
+<template>
+    <div class="relative">
+        <a class="target-burger" :class="modelValue && 'toggled'" @click="modelValue = !modelValue">
+            <ul class="buns">
+                <li class="bun"></li>
+                <li class="bun"></li>
+            </ul>
+        </a>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.target-burger li {
+    display: block;
+}
+
+/*小屏汉堡包*/
+a.target-burger {
+    cursor: pointer;
+    width: 2.5rem;
+    height: 2.5rem;
+    display: block;
+    -webkit-transition: all 0.5s ease;
+    -moz-transition: all 0.5s ease;
+    -o-transition: all 0.5s ease;
+    transition: all 0.5s ease;
+}
+
+a.target-burger.toggled ul.buns li.bun {
+    -webkit-transform: rotate(45deg) translateZ(0);
+    transform: rotate(45deg) translateZ(0);
+}
+
+a.target-burger.toggled ul.buns li.bun:last-child {
+    -webkit-transform: rotate(-45deg) translateZ(0);
+    transform: rotate(-45deg) translateZ(0);
+}
+
+a.target-burger ul.buns {
+    width: 1.25rem;
+    height: 1.25rem;
+    list-style: none;
+    margin: -0.8125rem 0 0 -0.3125rem;
+    padding: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transition: -webkit-transform 1s cubic-bezier(0.23, 1, 0.32, 1), color 1s cubic-bezier(0.23, 1, 0.32, 1);
+    transition: transform 1s cubic-bezier(0.23, 1, 0.32, 1), color 1s cubic-bezier(0.23, 1, 0.32, 1);
+    -webkit-transform: translateZ(0);
+    transform: translateZ(0);
+}
+
+a.target-burger ul.buns li.bun {
+    width: 100%;
+    height: 2px;
+    background-color: #000;
+    position: absolute;
+    top: 65%;
+    -webkit-transform: translateY(-3.75px) translateZ(0);
+    transform: translateY(-3.75px) translateZ(0);
+    -webkit-transition: -webkit-transform 1s cubic-bezier(0.23, 1, 0.32, 1), background-color 1s cubic-bezier(0.23, 1, 0.32, 1);
+    transition: transform 1s cubic-bezier(0.23, 1, 0.32, 1), background-color 1s cubic-bezier(0.23, 1, 0.32, 1);
+}
+
+a.target-burger ul.buns li.bun:last-child {
+    -webkit-transform: translateY(4.75px) translateZ(0);
+    transform: translateY(4.75px) translateZ(0);
+}
+</style>
