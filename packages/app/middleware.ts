0cc1d687bdd0c29ce620ddf9480ba6702b5150e1
Ted Xu <hi@xzd.me>
2024-01-21 17:23:27 +0800

    chore: tide up

--- packages/app/middleware.ts
+++ packages/app/middleware.ts
@@ -1,11 +1,18 @@
 /* eslint-disable node/prefer-global/process */
+
+/**
+ * This file is a Vercel Edge Function middleware.
+ * It rewrites requests to the Sanity Studio to the correct domain.
+ */
+
 import { rewrite } from '@vercel/edge'
 
 export default function middleware(request: Request) {
     const url = new URL(request.url)
 
     if (url.pathname.startsWith('/studio')) {
-        const baseUrl = process.env.SANITY_STUDIO_SITE_DOMAIN || ''
-        return rewrite(new URL(url.pathname, baseUrl))
+        const baseUrl = process.env.SANITY_STUDIO_SITE_DOMAIN
+        if (baseUrl)
+            return rewrite(new URL(url.pathname, baseUrl))
     }
 }
