299a00d95992c25f189040eb3619b8902da3e9e7
许作栋 <hi@xzd.me>
2024-03-10 22:35:37 +0800

    chore: update loading indicator color

--- packages/app/app.vue
+++ packages/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <nuxt-loading-indicator />
+    <nuxt-loading-indicator color="repeating-linear-gradient(to right,#e06ab8 0%,#e09361 100%)" />
     <nuxt-layout>
         <nuxt-page />
     </nuxt-layout>
