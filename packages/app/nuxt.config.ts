883b05a6187ee3911258cbd1f5c8c1932ee647c6
Ted Xu <hi@xzd.me>
2024-04-24 16:27:21 +0800

    chore: try fix Vercel middleware error

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -77,6 +77,7 @@ export default defineNuxtConfig({
         tsConfig: {
             compilerOptions: {
                 moduleResolution: 'node',
+                verbatimModuleSyntax: false,
             },
         },
     },
