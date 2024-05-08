b0d6e9e53e7e939aa7fa836ff6996f0a4b35317e
Ted Xu <hi@xzd.me>
2024-05-08 17:26:34 +0800

    refactor: ui layer now based on shadcn-vue

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,13 @@
 import eslintConfig from '@sanomics/eslint-config'
 
-export default eslintConfig()
+export default eslintConfig({}, {
+    files: ['packages/ui/components/ui/**/*.{vue,ts}'],
+    rules: {
+        'import/order': ['off'],
+        'style/indent': ['warn', 2],
+        'ts/no-unused-vars': ['off'],
+        'vue/html-indent': ['warn', 2],
+        'vue/html-self-closing': ['off'],
+        'vue/component-name-in-template-casing': ['off'],
+    },
+})
